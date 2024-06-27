ae471ae542cbbe773e53c7adce1aa5b0154101a8
Hordunlarmy <Hordunlarmy@gmail.com>
2024-06-27 13:31:37 +0100

    refactor(HolbertonCourse): simplify students setter logic

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -29,13 +29,12 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (
-      Array.isArray(value) &&
-      value.every((student) => typeof student === 'string')
-    ) {
-      this._students = value;
-    } else {
+    if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
     }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
   }
 }
