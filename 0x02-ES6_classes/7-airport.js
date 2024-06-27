f99e778290cbed73c4d613692a1a4b8d8caefa2b
Hordunlarmy <Hordunlarmy@gmail.com>
2024-06-27 15:00:48 +0100

    feat(Airport class): Add new ES6 class with getters and setters

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,28 @@
+export default class Airport {
+  constructor(name, code) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') throw new TypeError('code is not a string');
+    this._code = value;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('name is not a string');
+    this._name = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `(${this.code})`;
+  }
+}
